d4d454f9b5112dcb2e9581229141abea47d25c2b
RLMurta <rafaellmurta@hotmail.com>
2023-10-21 14:03:57 -0300

    Rule separation implementation

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 from read_file import ReadFile
 
 cnf_file = ReadFile('cnf_file.cnf')
-cnf_file.read()
-print(cnf_file.file.splitlines())
--- read_file.py
+++ read_file.py
@@ -3,10 +3,20 @@ from normal_forms import ChomskyNormalForm, SecondNormalForm
 class ReadFile:
     def __init__(self, file_name):
         self.file_name = file_name
+        self.read()
+        self.separate_rules()
 
     def read(self):
         with open(self.file_name) as f:
             self.file = f.read()
+
+    def separate_rules(self):
+        rules = self.file.splitlines()
+        treated_rules = dict()
+        for rule in rules:
+            variable = rule.split('->')[0].replace(" ", "")
+            treated_rules[variable] = rule.split('->')[1].replace(" ", "").split('|')
+        self.rules = treated_rules
 
     def read_cnf(self):
         pass
