9ba75b0c27e3d79774da2d637ee8f7faa26d824e
RLMurta <rafaellmurta@hotmail.com>
2023-12-02 15:22:59 -0300

    Subindo alterações esteticas da main.py

--- main.py
+++ main.py
@@ -16,42 +16,41 @@ print('Gramatica de entrada:')
 grammar.print()
 
 # Aplicação da Forma Normal de Chomsky
-chomsky_normal_form_start_time = time.time()
+cyk_start_time = time.time()
 cfgToCnf(grammar)
-chomsky_normal_form_total_time = time.time() - chomsky_normal_form_start_time
-
 # Verificação se a entrada pertence à gramática usando o algoritmo CYK
 isPresent = Cyk.run(grammar, entrada)
+cyk_total_time = time.time() - cyk_start_time
 
 if isPresent:
-    print(f'Utilizando a Forma de Chomsky Normal a frase de entrada: "{entrada}" pertence à gramática')
+    print(f'Utilizando CYK: "{entrada}" pertence à gramática')
 else:
-    print(f'Utilizando a Forma de Chomsky Normal a frase de entrada: "{entrada}" não pertence à gramática')
+    print(f'Utilizando CYK: "{entrada}" não pertence à gramática')
 
 # Impressão da Forma Normal de Chomsky
-print('Forma Normal de Chomsky:')
-grammar.print()
+# print('Forma Normal de Chomsky:')
+# grammar.print()
 
 # Reinicialização da gramática para aplicação da Segunda Forma Normal de Chomsky
 grammar2 = Gramatica()
 grammar2.readGramatica(filename)
 
 # Aplicação da Segunda Forma Normal de Chomsky
-second_normal_form_start_time = time.time()
+modified_cyk_start_time = time.time()
 cfgTo2nf(grammar2)
 anulaveis = methods.anulavel(grammar2)
 isPresent2 = ModifiedCyk.run(grammar2, entrada, anulaveis)
-second_normal_form_total_time = time.time() - second_normal_form_start_time
+modified_cyk_total_time = time.time() - modified_cyk_start_time
 
 if isPresent2:
-    print(f'Utilizando a Segunda Forma Normal de Chomsky a frase de entrada: "{entrada}" pertence à gramática')
+    print(f'Utilizando CYK Modificado: "{entrada}" pertence à gramática')
 else:
-    print(f'Utilizando a Segunda Forma Normal de Chomsky a frase de entrada: "{entrada}" não pertence à gramática')
+    print(f'Utilizando CYK Modificado: "{entrada}" não pertence à gramática')
 
 # Impressão da Segunda Forma Normal de Chomsky
-print('Segunda Forma normal de Chomsky:')
-grammar2.print()
+# print('Segunda Forma normal de Chomsky:')
+# grammar2.print()
 
 # Exibição dos tempos de execução
-print('Tempo de execução da Forma Normal de Chomsky:', round(chomsky_normal_form_total_time, 5))
-print('Tempo de execução da segunda Forma normal de Chomsky', round(second_normal_form_total_time, 5))
+print('Tempo de execução CYK:', round(cyk_total_time, 5))
+print('Tempo de execução CYK Modificado', round(modified_cyk_total_time, 5))
